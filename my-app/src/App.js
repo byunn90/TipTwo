80c9e4132983c69013e04cf6245cd04b5a92c235
byunn90 <kayhanturk90@Live.com>
2024-01-23 17:06:40 +1100

    aligned input and h1 together

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,24 @@
 import "./index.css";
+import { useState } from "react";
 import React from "react";
 
 export default function App() {
   return <Calculator />;
 }
-
+// Renders the calculator
 function Calculator() {
-  return <h1 className="hh">aaaaaa</h1>;
+  const [billValue, SetBillValue] = useState("");
+  // const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    SetBillValue(e.target.value);
+  }
+
+  return (
+    <div className="form-container">
+      <h1>How Much was the bill?</h1>
+      <input text="text" value={billValue} onChange={handleSubmit} />
+      <h1>{SetBillValue}</h1>
+    </div>
+  );
 }
