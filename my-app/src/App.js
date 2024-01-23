da7c03877e69ef748771db1347a7ab7205239829
byunn90 <kayhanturk90@Live.com>
2024-01-23 16:47:28 +1100

    ts not working

---
+++ my-app/src/App.js
@@ -0,0 +1,10 @@
+import "./index.css";
+import React from "react";
+
+export default function App() {
+  return <Calculator />;
+}
+
+function Calculator() {
+  return <h1 className="hh">aaaaaa</h1>;
+}
