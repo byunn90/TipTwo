78b1b41a251923db2f981de7e8893eb8cee24f60
byunn90 <kayhanturk90@Live.com>
2024-01-25 13:24:50 +1100

    finished 1 person now second person needs to be done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,38 +5,70 @@ import React from "react";
 export default function App() {
   return <TipCalculator />;
 }
+
 // Renders the calculator
 function TipCalculator() {
-  const [billValue, SetBillValue] = useState("");
+  const [billValue, setBillValue] = useState("");
   const [percentage1, setPercentage1] = useState(0);
-  const [percentage2, setPercentage2] = useState(0);
-  // const [quantity, setQuantity] = useState(1);
 
-  function handleSubmit(e) {
-    SetBillValue(e.target.value);
-    console.log(SetBillValue);
-  }
-
-  function Enjoyed(option) {
-    return (
-      <label>
-        <select>
-          <option></option>
-          <option></option>
-          <option></option>
-          <option></option>
-        </select>
-      </label>
-    );
+  function resetBill() {
+    setBillValue("");
+    setPercentage1(0);
   }
 
   return (
-    <>
-      <div className="form-container">
-        <h1>How Much was the bill?</h1>
-        <input text="text" value={billValue} onChange={handleSubmit} />
-        <h1>{billValue}</h1>
-      </div>
-    </>
+    <div>
+      <BillInput billValue={billValue} setBillValue={setBillValue} />
+      <PercentageTip
+        selectedPercentage={percentage1}
+        setPercentage1={setPercentage1}
+      />
+      <CalculatedPercentageTip
+        billValue={billValue}
+        selectedPercentage={percentage1}
+        resetBill={resetBill}
+      />
+    </div>
+  );
+}
+
+function BillInput({ billValue, setBillValue }) {
+  return (
+    <div>
+      <label>How much was the bill?</label>
+      <input
+        type="text"
+        value={billValue}
+        onChange={(e) => setBillValue(Number(e.target.value))}
+      />
+    </div>
+  );
+}
+
+function PercentageTip({ selectedPercentage, setPercentage1 }) {
+  return (
+    <div>
+      <label>How did you like the Service?</label>
+      <select
+        value={selectedPercentage}
+        onChange={(e) => setPercentage1(Number(e.target.value))}
+      >
+        <option value="0">dissatisfied (0%)</option>
+        <option value="5">It was Ok (5%)</option>
+        <option value="10">It was good (10%)</option>
+        <option value="20">Absolutely Amazing (20%)</option>
+      </select>
+    </div>
+  );
+}
+
+function CalculatedPercentageTip({ billValue, selectedPercentage, resetBill }) {
+  const calculatedTip = (billValue * selectedPercentage) / 100;
+
+  return (
+    <div>
+      <h1>Your tip will be: {calculatedTip + billValue}</h1>
+      <button onClick={resetBill}>Reset</button>
+    </div>
   );
 }
